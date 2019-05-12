357244fc24638952fa8ca5cdf83ebd2b6ec13ea7
acelga18 <36709325+acelga18@users.noreply.github.com>
2019-05-12 19:50:20 +0200

    Add files via upload

---
+++ PlantasVsZombis-master/src/modelo/Girasol.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class Girasol extends Planta {
+    static int soles = 0, cantidad= 0;
+
+    static public void reiniciaAtributos() {
+        Girasol.cantidad=0;
+        Girasol.soles=0;
+    }
+
+    public Girasol(int fila,int columna) {
+        super(fila,columna,"G(1)");
+        cantidad+=1;
+    }
+    
+    static public int getSoles() {
+        soles+=cantidad;
+        return soles+50;
+    }
+}
+
